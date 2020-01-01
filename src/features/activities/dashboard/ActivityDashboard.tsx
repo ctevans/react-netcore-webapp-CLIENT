f1a9b3265c192becb5fbbc7d379a7e8e466d21cd
Cole Thomas Evans <cevans2@ualberta.ca>
2020-01-01 09:37:26 -0700

    Creation of ActivityForm component

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid, List } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { ActivityList } from './ActivityList';
 import { ActivityDetails } from '../details/ActivityDetails';
+import { ActivityForm } from '../form/ActivityForm';
 
 interface IProps {
     activities: IActivity[]
@@ -12,10 +13,11 @@ export const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
     return (
         <Grid>
             <Grid.Column width={10}>
-                <ActivityList activities={activities}/>
+                <ActivityList activities={activities} />
             </Grid.Column>
             <Grid.Column width={6}>
-                <ActivityDetails/>
+                <ActivityDetails />
+                <ActivityForm />
             </Grid.Column>
         </Grid>
     )
