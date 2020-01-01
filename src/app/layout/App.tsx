3caad7a15413d20201732d509acb9de5fd11f12a
Cole Thomas Evans <cevans2@ualberta.ca>
2020-01-01 08:18:38 -0700

    Making it look slightly better

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Header, Icon, List } from 'semantic-ui-react';
+import { Header, Icon, List, Container } from 'semantic-ui-react';
 import axios from 'axios';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
@@ -18,11 +18,13 @@ const App = () => {
     <div>
       <NavBar />
 
-      <List>
-        {activities.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
-        ))}
-      </List>
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </List>
+      </Container>
     </div>
   );
 }
