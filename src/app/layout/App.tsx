353e95827ca5bf2b282b2b026239816db4d0ff14
Cole Thomas Evans <cevans2@ualberta.ca>
2020-01-01 07:59:10 -0700

    Moving from class component and giving functional components and hooks a real good try.

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header, Icon, List } from 'semantic-ui-react';
 import axios from 'axios';
 import { IActivity } from '../models/activity';
 
-interface IState {
-  activities: IActivity[];
-}
+const App = () => {
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: []
-  }
-
-  componentDidMount() {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  useEffect(() => {
     axios.get<IActivity[]>('http://localhost:5000/api/activities')
       .then((response) => {
-        this.setState({
-          activities: response.data
-        })
+        setActivities(response.data)
       })
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='plug' />
-          <Header.Content>Activity Sample App</Header.Content>
-        </Header>
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='plug' />
+        <Header.Content>Activity Sample App</Header.Content>
+      </Header>
 
-        <List>
-          {this.state.activities.map((activity) => (
-            <li key={activity.id}>{activity.title}</li>
-          ))}
-        </List>
-      </div>
-    );
-  }
+      <List>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
