78c4c73bcdb08d55d1e490ec42f7c48a1a5d9f22
Cole Thomas Evans <cevans2@ualberta.ca>
2020-01-02 13:34:21 -0700

    individual activity list item

--- src/app/stores/activityStore.ts
+++ src/app/stores/activityStore.ts
@@ -13,8 +13,18 @@ class ActivityStore {
     @observable target = '';
 
     @computed get activitiesByDate() {
-        return Array.from(this.activityRegistry.values())
-            .sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+    }
+
+    groupActivitiesByDate(activities: IActivity[]) {
+        const sortiedActivities = activities.sort(
+            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        )
+        return Object.entries(sortiedActivities.reduce((activities, activity) => {
+            const date = activity.date.split('T')[0];
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as { [key: string]: IActivity[] }));
     }
 
     @action loadActivities = async () => {
