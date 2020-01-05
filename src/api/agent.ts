d1375ac9a667fe7694eca01555a14887cd2a6fea
Cole Thomas Evans <cevans2@ualberta.ca>
2020-01-05 10:33:55 -0700

    incorporate JWT token into headers for axious requests.

--- src/api/agent.ts
+++ src/api/agent.ts
@@ -6,6 +6,14 @@ import { IUser, IUserFormValues } from '../app/models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
+axios.interceptors.request.use((config) => {
+    const token = window.localStorage.getItem('jwt');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
 axios.interceptors.response.use(undefined, error => {
     //Network errors.
     if (error.message === 'Network Error' && !error.response) {
