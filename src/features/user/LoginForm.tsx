ffa811e7a4d82ab022a483455fc28b4526c6dee8
Cole Thomas Evans <cevans2@ualberta.ca>
2020-01-05 09:00:46 -0700

    added login form

---
+++ src/features/user/LoginForm.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Form as FinalForm, Field, Form } from 'react-final-form';
+import TextInput from '../../app/common/form/TextInput';
+import { Button } from 'semantic-ui-react';
+
+export const LoginForm = () => {
+    return (
+        <FinalForm
+            onSubmit={(values) => console.log(values)}
+            render={({ handleSubmit }) => (
+                <Form onSubmit={handleSubmit}>
+                    <Field
+                        name='email'
+                        component={TextInput}
+                        placeholder='Email' />
+                    />
+                <Field
+                        name='password'
+                        component={TextInput}
+                        placeholder='Password'
+                        type='password' />
+                    <Button positive content="Login" />
+                    />
+            </Form>
+            )}
+        />
+    )
+}
