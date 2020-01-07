bbb2962fd4b64537b0147d2e193991786658aacc
Cole Thomas Evans <cevans2@ualberta.ca>
2020-01-07 00:33:22 -0700

    added react cropper D:

--- src/app/common/photoUpload/PhotoUploadWidget.tsx
+++ src/app/common/photoUpload/PhotoUploadWidget.tsx
@@ -3,9 +3,11 @@ import React, { Fragment, useState, useEffect } from 'react';
 import { Header, Grid, Image } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import PhotoWidgetDropzone from './PhotoWidgetDropzone';
+import PhotoWidgetCropper from './PhotoWidgetCropper';
 
 const PhotoUploadWidget = () => {
     const [files, setFiles] = useState<any[]>([]); //INTENTIONALLY BYPASS TYPE CHECKING.
+    const [image, setImage] = useState<Blob | null>(null);
 
     //Cleanup to prevent memory leak from image.
     //Removes as soon as image is put into the URL.
@@ -26,13 +28,17 @@ const PhotoUploadWidget = () => {
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
                     <Header sub color='teal' content='Step 2 - Resize image' />
+                    {
+                        files.length > 0 &&
+                        <PhotoWidgetCropper setImage={setImage} imagePreview={files[0].preview} />
+                    }
                 </Grid.Column>
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
                     <Header sub color='teal' content='Step 3 - Preview & Upload' />
                     {
                         files.length > 0 &&
-                        <Image src={files[0].preview} />
+                        <div className='img-preview' style={{minHeight: '200px', overflow: 'hidden'}}/>
                     }
                 </Grid.Column>
             </Grid>
